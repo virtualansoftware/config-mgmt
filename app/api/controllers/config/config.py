3ee4e71863df79c5639297c22a6bc560bb8f241b
Balchamy Socretees <saamy2023@hotmail.com>
2024-08-18 20:36:37 -0500

    Added Repo and Servvice files

--- app/api/controllers/config/config.py
+++ app/api/controllers/config/config.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException
-import boto3
-import os
-import json
+#import boto3
+#import os
+#import json
 from botocore.exceptions import ClientError
 from utils.log import log
 
 from jinja2 import UndefinedError
 
-from settings import settings
+#from settings import settings
 from app.schemas.config.ConfigSchema import ConfigSchema
-from app.datagenerator.process_template import apply_configuration
+#from app.datagenerator.process_template import apply_configuration
 
-from services.AuthService import AuthService
 
+#from services.AuthService import AuthService
+from services.TemplateGeneration import ReadConfiguration,GenerateConfiguration,GetConfig,SaveConfiguration
 
 router = APIRouter(
     tags=["Config Api"],
@@ -24,11 +25,7 @@ router = APIRouter(
 async def save_configuration(config_info: ConfigSchema):
     try:
         log.debug(config_info)
-        s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY,
-                                 aws_secret_access_key=settings.AWS_SECRET_KEY)
-        json_object = json.dumps(config_info.configMap, indent=4)
-        s3_client.put_object(Body=json_object, Bucket=settings.STORAGE_BUCKET_NAME,
-                             Key=config_info.envName + '/' + config_info.keyId + '.json')
+        SaveConfiguration(config_info)
     except UndefinedError as e:
         print("Error Occurred and Handled" + e.message)
         return "Error Occurred and Handled " + e.message
@@ -43,13 +40,8 @@ async def save_configuration(config_info: ConfigSchema):
 
 @router.get("/", response_model=ConfigSchema)
 async def read_configuration(env_name: str, key_id: str):
-    try:
-        s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY,
-                                 aws_secret_access_key=settings.AWS_SECRET_KEY)
-        s3_response_object = s3_client.get_object(Bucket=settings.STORAGE_BUCKET_NAME,
-                                                  Key=env_name + '/' + key_id + '.json')
-        object_content = s3_response_object['Body'].read()
-        configInfo = ConfigSchema(json.loads(object_content), key_id, env_name)
+    try:        
+        configInfo = ReadConfiguration(env_name,key_id)       
     except UndefinedError as e:
         print("Error Occurred and Handled" + e.message)
         return "Error Occurred and Handled " + e.message
@@ -63,28 +55,14 @@ async def read_configuration(env_name: str, key_id: str):
 
 
 async def get_config(env_name, key_id):
-    s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY,
-                             aws_secret_access_key=settings.AWS_SECRET_KEY)
-    s3_response_object = s3_client.get_object(Bucket=settings.STORAGE_BUCKET_NAME,
-                                              Key=env_name + '/' + key_id + '.json')
-    object_content = s3_response_object['Body'].read()
+    object_content = GetConfig(env_name,key_id)
     return object_content
 
 
 @router.post("/generate", response_model=dict)
 async def generate_configuration(env_name: str, key_id: str, template_file_name: str):
     try:
-        s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY,
-                                 aws_secret_access_key=settings.AWS_SECRET_KEY)
-        s3_response_object = s3_client.get_object(Bucket=settings.STORAGE_BUCKET_NAME,
-                                                  Key=env_name + '/' + key_id + '.json')
-        object_content = s3_response_object['Body'].read()
-        TEMPLATE_FILE = 'config-mgmt/templates/applications/' + key_id + '/' + template_file_name
-        s3_template_object = s3_client.get_object(Bucket=settings.STORAGE_BUCKET_NAME, Key=TEMPLATE_FILE)
-        template_file_content = s3_template_object['Body'].read()
-        templateGenerated = apply_configuration(key_id, json.loads(object_content), template_file_content.decode('UTF-8'))
-        s3_client.put_object(Body=templateGenerated, Bucket=settings.STORAGE_BUCKET_NAME,
-                             Key=env_name + '/' + key_id + '/generated/' + template_file_name)
+        templateGenerated= GenerateConfiguration(env_name,key_id,template_file_name)
         return {"template_generated": templateGenerated }
     except UndefinedError as e:
         print("Error Occurred and Handled" + e.message)
---
+++ repository/TemplateRepo.py
@@ -0,0 +1,36 @@
+import json
+from settings import settings
+import boto3
+from app.schemas.config.ConfigSchema import ConfigSchema
+
+def GetS3Client():
+     return boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY,
+                                 aws_secret_access_key=settings.AWS_SECRET_KEY)
+    
+async def SaveConfigurationRepo(config_info: ConfigSchema):
+    s3_client = GetS3Client()
+    json_object = json.dumps(config_info.configMap, indent=4)
+    s3_client.put_object(Body=json_object, Bucket=settings.STORAGE_BUCKET_NAME,
+                             Key=config_info.envName + '/' + config_info.keyId + '.json')
+
+async def GetConfigurationRepo(env_name: str, key_id: str):
+   # s3_client = GetS3Client()
+    #s3_response_object = s3_client.get_object(Bucket=settings.STORAGE_BUCKET_NAME,Key=env_name + '/' + key_id + '.json')
+    #return s3_response_object['Body'].read()
+    return GetObjectRepo(env_name + '/' + key_id + '.json')
+
+async def GetConfigurationTemplateRepo(template_file: str):
+   # s3_client = GetS3Client()
+   # s3_response_object = s3_client.get_object(Bucket=settings.STORAGE_BUCKET_NAME, Key=template_file)
+   # return s3_response_object['Body'].read()
+    return GetObjectRepo(template_file)
+
+async def GetObjectRepo(file: str):
+    s3_client = GetS3Client()
+    s3_response_object = s3_client.get_object(Bucket=settings.STORAGE_BUCKET_NAME, Key=file)
+    return s3_response_object['Body'].read()
+
+async def GenerateObjectRepo(env_name: str, key_id: str,template_file_name: str,templateGenerated: str):
+     s3_client = GetS3Client()
+     s3_client.put_object(Body=templateGenerated, Bucket=settings.STORAGE_BUCKET_NAME,
+    Key=env_name + '/' + key_id + '/generated/' + template_file_name)
